288686e2b471091e60fac398b71dd600fed5f10e
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2019-11-22 07:57:05 +0100

    add ErrorReponse when user not in project

--- utils/checkUserRole.js
+++ utils/checkUserRole.js
@@ -7,7 +7,7 @@ const checkRole = async (projectId,req, next) =>{
     if(!project){
         return next(new ErrorResponse(`Project not found with id of ${projectId}`,404));
       }
-
+    
      //Make sure user is project owner
      if (project.owner.toString() !==req.user.id && req.user.status !=='admin') {
         //If user role is administrator
@@ -18,9 +18,15 @@ const checkRole = async (projectId,req, next) =>{
                 userRole=member.role
             }  
         }); 
+        
 
-        if(!userRole || userRole!=='administrator')
-        return next(new ErrorResponse(`User ${req.user.email } is not authorized to this route`,401));
+        if(!userRole || userRole!=='administrator'){
+            return next(new ErrorResponse(`User ${req.user.email } is not authorized to this route`,401));
+        }
+        else{
+            return next ( new ErrorResponse(`User ${req.user.email } is not in this project`,401));
+        }
+        
        
     }
 }
