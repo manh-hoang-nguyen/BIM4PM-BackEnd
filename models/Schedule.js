ae30ce88d965731bb82f40f43ac04ef76db6eff3
Manh Hoang NGUYEN <Nguyenhoang56ksgt@gmail.com>
2019-12-12 17:48:30 +0100

    add schedule schema

---
+++ models/Schedule.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ScheduleSchema = mongoose.Schema({
+    project: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Project',
+        required: [true, 'Please add project id']
+      }, 
+      guid: {
+        type: String,
+        required: [true, 'Please add schedule guid ']
+      },
+      name:{
+          type: String,
+          require:[true, 'Please add the name of schedule']
+      }
+});
+
+ScheduleSchema.index({ project: 1}); //indexing
+module.exports = mongoose.model('Schedule', ScheduleSchema);
