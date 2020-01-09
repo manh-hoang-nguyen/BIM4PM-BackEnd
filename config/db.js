6006677f3f833c09a2b1e853c9121694438b72da
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2020-01-09 08:01:10 +0100

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB=async()=>{
-    const  conn=await mongoose.connect(process.env.MONGO_URI,{
+    const  conn=await mongoose.connect(process.env.MONGO_URI_LOCAL,{
         useNewUrlParser:true,
         useCreateIndex:true,
         useFindAndModify:false,
